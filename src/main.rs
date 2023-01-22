faab185c0901080f3d075201442f4b70fcf639cc
musergi <sergi.mus@gmail.com>
2023-01-22 18:09:13 +0100

    Added command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+use clap::Parser;
 use hawkeye::generated::hawkeye_service_server::HawkeyeServiceServer;
 use hawkeye::HawkeyeServiceImpl;
 use systemstat::{Platform, System};
 use tonic::transport::Server;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    address: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
     let system = System::new();
     let service = HawkeyeServiceImpl::new(system);
-    let addr = "[::1]:50051".parse()?;
+    let addr = args.address.parse()?;
     Server::builder()
         .add_service(HawkeyeServiceServer::new(service))
         .serve(addr)
